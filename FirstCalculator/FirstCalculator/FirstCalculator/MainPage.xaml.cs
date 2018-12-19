7e2ac119a012fe704b2dc49d62536d0e567b825d
Unknown <hdagon01@gmail.com>
2018-12-19 11:22:26 -0500

    Initialized Project

---
+++ FirstCalculator/FirstCalculator/FirstCalculator/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FirstCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        private double result = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Pressed(object sender, EventArgs e)
+        {
+            Button b = (sender as Button);
+            //for num press, Multiply by 10, add num
+            //for operator press, result = input, input = 0
+            int input = 0;
+
+            
+        }
+
+        private void Operator_Pressed(object sender, EventArgs e)
+        {
+            Button u = (sender as Button);
+
+        }
+    }
+}
